c5bd101d307e76aeacd1d7e6d52be733b4c826c2
sam <samuelapicella@hotmail.com>
2017-06-12 16:54:32 +0200

    1.la bozza dello scheletro 2.termine bozza dello scheletro

---
+++ src/Accoppiatore.java
@@ -0,0 +1,15 @@
+//ad ogni coppia che si incontra si crea un accoppiatore
+//in questo modo al main non rimane che gestire le generazioni
+public class Accoppiatore extends Thread{
+    Maschio maschio;
+    Femmina femmina;
+    public Accoppiatore(Maschio maschio, Femmina femmina){
+        this.maschio = maschio;
+        this.femmina = femmina;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
